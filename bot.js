36fa05111fdb9cfa1e7e911488280409f917bc80
jameykinsel <jkinsel@researchnow.com>
2018-03-21 12:11:58 -0500

    added removerow detail to help command

--- bot.js
+++ bot.js
@@ -154,6 +154,7 @@ var help = function (channelID) {
    str1 += "!list <quantity> <name> <currency> <price> --- List new items to inventory \n";
    str1 += "!sold <quantity> <name> <currency> --- Track item sales \n";
    str1 += "!remove <quantity> <name> <currency> --- Remove items from inventory \n";
+   str1 += "!removerow <name> <currency> <price> --- Delete row from table. \n"
    str1 += "!updateprice <name> <currency> <price> --- Update price for existing row \n";
    str1 += "!total <currency> --- Sum total sold by currency \n"
    str1 += "!clearinv --- Clears all current inventory \n";
